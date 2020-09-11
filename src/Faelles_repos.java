c5751cba7aec0fbd00afdf01f88d9af2eb6611dc
jenna-P <jenn2464@edu.easj.dk>
2020-09-11 11:27:17 +0200

    Jenna har tilføjet

--- src/Faelles_repos.java
+++ src/Faelles_repos.java
@@ -2,6 +2,7 @@ public class Faelles_repos {
     public static void main(String[] args) {
         System.out.println("This is our first shared repo :)");
         System.out.println("Hello This is Jenna");
+        System.out.print("Jenna");
     }
 
 
