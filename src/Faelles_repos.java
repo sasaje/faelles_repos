9a8d15393c26e30e555ce6dca732fd14d989b4cc
MalteZea <70883688+MalteZea@users.noreply.github.com>
2020-09-11 11:32:16 +0200

    Jeg har tilføjet mit navn

--- src/Faelles_repos.java
+++ src/Faelles_repos.java
@@ -2,7 +2,10 @@ public class Faelles_repos {
     public static void main(String[] args) {
         System.out.println("This is our first shared repo :)");
         System.out.println("Hello This is Jenna");
-        System.out.print("Jenna");
+        System.out.println("Jenna");
+
+        var member = "Malte";
+        System.out.println(member + " is a capable member of this team");
     }
 
 
